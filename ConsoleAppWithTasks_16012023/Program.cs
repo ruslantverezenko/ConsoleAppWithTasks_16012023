b5475dc44a87b8465ed5de721d6060cd9d528d1b
Ruslan Tverezenko <68191748+ruslantverezenko@users.noreply.github.com>
2023-01-16 23:40:54 +0300

    ConsoleAppWithTasks_16012023
    1. Запросить имя пользователя. Вывести Hello, [имя пользователя].
    
    2. Запросить у пользователя два целых числа и сохранить в двух переменных. Вывести значения.
    Обменять значения переменных: например, если в переменной x было записано число 3, а в y число 5, сделать так, чтобы в y стало 3, а в x стало 5. Вывести значения после обмена.
    
    3. Запросить у пользователя целое число. Вывести количество цифр числа. Например, в числе 156 - 3 цифры.

---
+++ ConsoleAppWithTasks_16012023/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithTasks_16012023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Запросить имя пользователя. Вывести Hello, [имя пользователя].
+            Task1();
+            /*2. Запросить у пользователя два целых числа и сохранить в двух переменных. Вывести значения.
+             * Обменять значения переменных: например, если в переменной x было записано число 3,
+             * а в y число 5, сделать так, чтобы в y стало 3, а в x стало 5.
+             * Вывести значения после обмена.
+             */
+            Task2();
+            //3. Запросить у пользователя целое число. Вывести количество цифр числа. Например, в числе 156 - 3 цифры.
+            Task3();
+            Console.ReadLine();
+        }
+        static void Task1()
+        {
+            Console.Write("Введите имя пользователя: ");
+            string username = Console.ReadLine();
+            Console.WriteLine($"Hello, {username}");
+        }
+        static void Task2()
+        {
+            Console.Write("Введите целое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите целое число: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Первое число: {num1} Второе число: {num2}");
+
+            int temp = num2;
+            num2 = num1;
+            num1 = temp;
+            Console.WriteLine($"Первое число: {num1} Второе число: {num2}");
+        }
+        static void Task3()
+        {
+            Console.Write("Введите целое число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string numString = num.ToString();
+            Console.WriteLine($"Количество цифр числа {num} = {numString.Length}");
+
+        }
+    }
+}
+
---
+++ ConsoleAppWithTasks_16012023/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleAppWithTasks_16012023")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppWithTasks_16012023")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("001f587e-1995-4d70-beae-4795cb8d87a4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
